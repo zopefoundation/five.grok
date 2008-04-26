dc33917d8fa363bb9b59f1b28a94998c476d8762
Godefroid Chapelle <gotcha@bubblenet.be>
2008-04-26 17:16:08 +0000

    import all grokcore directives in five.grok namespace

--- src/five/grok/__init__.py
+++ src/five/grok/__init__.py
@@ -1,6 +1,10 @@
 # Import adapter and utility support from grokcore.component.
 from grokcore.component.components import GlobalUtility, Adapter
-from grokcore.component.directive import name, provides, context
+
+from grokcore.component.directive import name, provides
+from grokcore.component.directive import order, direct
+from grokcore.component.directive import context, title, baseclass
+
 from zope.interface import implements
 
 from components import View
