ac41dc4dbb3cb9502a2f4492e94ae1293901cbbf
eric casteleijn <eric@infrae.com>
2008-05-03 15:56:37 +0000

    cleaned up the imports with faassen's blessing

--- src/five/grok/__init__.py
+++ src/five/grok/__init__.py
@@ -1,12 +1,6 @@
 # Import adapter and utility support from grokcore.component.
-from grokcore.component.components import GlobalUtility, Adapter, MultiAdapter
 
-from grokcore.component.directive import name, provides
-from grokcore.component.directive import order, direct
-from grokcore.component.directive import context, title, baseclass
-
-from zope.interface import implements
-from zope.component import adapts
+from grokcore.component import *
 
 from components import View
 from directive import require
