56b6dde539d3e8dbafbf254136db430ab67b12f8
Martin Aspeli <optilude@gmail.com>
2008-08-04 00:13:25 +0000

    Add comment so people know why we do an import-as

--- src/five/grok/__init__.py
+++ src/five/grok/__init__.py
@@ -3,4 +3,6 @@ from grokcore.security import *
 from grokcore.view import *
 
 from five.grok.components import View, Model
+
+# Override the one from grokcore.view so that we get Zope 2 semantics
 from five.grok.components import ZopeTwoPageTemplate as PageTemplate
